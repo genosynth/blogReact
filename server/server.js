4e1698e6a792ac52d58ef6861e3848b31722c083
genosynth <jpgenovese@gmail.com>
2023-12-17 20:10:27 +0100

    delivering front end again

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ app.use(express.json())
 app.use('/app', routesUrls)
 
 
-/* app.use(express.static(path.join(__dirname + '/public' )))
+ app.use(express.static(path.join(__dirname + '/public' )))
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});  */
+});  
 
 mongoose.connect(process.env.DATABASE_ACCESS, (error, result) =>{
   if (error) {return console.log(error)}
