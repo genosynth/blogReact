12479c7593cd20dad18962ee265c7ff623b2401f
genosynth <jpgenovese@gmail.com>
2023-12-18 00:05:53 +0100

    test

--- server/server.js
+++ server/server.js
@@ -22,12 +22,20 @@ app.use(cors())
 app.use('/app', routesUrls)
 
 
-app.use(express.static(path.join(__dirname + '/public' )))
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});  
-
-
+// This code makes sure that any request that does not matches a static file
+// in the build folder, will just serve index.html. Client side routing is
+// going to make sure that the correct content will be loaded.
+app.use((req, res, next) => {
+  if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+      next();
+  } else {
+      res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+      res.header('Expires', '-1');
+      res.header('Pragma', 'no-cache');
+      res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  }
+});
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(port, () => {
