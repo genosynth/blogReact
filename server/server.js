aca5a017b3e8e96a7f60a7eae958e4235d904f01
genosynth <jpgenovese@gmail.com>
2023-12-17 14:49:12 +0100

    more server.js testing as routes are not working on the hosting platform

--- server/server.js
+++ server/server.js
@@ -17,10 +17,14 @@ mongoose.connect(process.env.DATABASE_ACCESS, (error, result) =>{
 
 
 app.use(cors())
-app.use(express.json())
+
 app.use('/app', routesUrls)
+app.use(express.json())
 
-
+app.use(express.static(path.join(__dirname + '/public' )))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});  
 
 
 
